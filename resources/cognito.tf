d8ab3cb6192fa4e4d83a3bf2fcd3bb9390180e46
lars-filemore <matthew.fry@spatialinsite.com.au>
2025-07-27 23:26:56 +0000

    Remove cognito

--- resources/cognito.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cognito_user_pool" "pool" {
-  name = "${var.org-short-name}-de-cognito-user-pool"
-
-}
-
-resource "aws_cognito_user_pool_domain" "pool" {
-  domain       = var.org-short-name
-  user_pool_id = aws_cognito_user_pool.pool.id
-}
